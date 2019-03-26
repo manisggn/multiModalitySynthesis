4470f2cbd7e2becbd42e487dcbd7aa0b758b1cc7
amirjaber <amirhossein.jaberzadeh@gmail.com>
2019-03-25 17:07:13 -0700

    ff

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ data.load()
 
 input_modalities= ['T1','T2']
 output_weights= { 'T2':1.0, 'concat':1.0}
-exp = Experiment(input_modalities, output_weights, './', data, latent_dim=4, 0)
+exp = Experiment(input_modalities, output_weights, './', data, latent_dim=4, False)
 exp.run(data)
