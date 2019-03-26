d50155ef9d53ea99b68c69fccd3244e016542986
amirjaber <amirhossein.jaberzadeh@gmail.com>
2019-03-25 20:29:56 -0700

    s

--- loader_multimodal.py
+++ loader_multimodal.py
@@ -211,7 +211,7 @@ class Data(object):
 
     def select_for_ids(self, modality, ids, as_array=True):
         assert modality in self.refDict
-        print self.refDict[modality].shape
+        print self.refDict[modality]
         data_ids = [self.refDict[modality][i] for i in ids]
 
         if as_array:
