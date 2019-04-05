4861af88d790e081cb163f468f2802b599966f7e
manprees <manpreet.singhsfs@gmail.com>
2019-04-04 18:13:30 -0700

    DFV

--- loader_multimodal.py
+++ loader_multimodal.py
@@ -168,6 +168,9 @@ class Data(object):
             if self.dataset == 'ISLES':
                 x = x[:, 0:-6, 34:-36]
 
+            if self.dataset == 'IXI':
+                X=X[:,0:-6,34:-36]
+
             if self.trim_and_downsample:
                 X[i] = block_reduce(x, block_size=(1, downsample, downsample), func=np.mean)
 
