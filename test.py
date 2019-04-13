d1e38d9f7eb5f4ca13232025e2decaa7c6a51e9e
amirjaber <amirhossein.jaberzadeh@gmail.com>
2019-04-12 20:32:15 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,10 +3,10 @@ from loader_multimodal import Data
 from runner import Experiment
 from keras import backend as K
 K.set_image_dim_ordering('th')
-data = Data('./data/', modalities_to_load=['T1','T2'], dataset='IXI', trim_and_downsample=False)
+data = Data('./data/', modalities_to_load=['T1'], dataset='IXI', trim_and_downsample=False)
 data.load()
 
-input_modalities= ['T1','T2']
+input_modalities= ['T1']
 output_weights= { 'T2':1.0, 'concat':1.0}
 exp = Experiment(input_modalities, output_weights, './', data, latent_dim=4, spatial_transformer=True)
 exp.run(data)
