42053610ca49201508fecd8ee5ed94f8f4121bf2
amirjaber <amirhossein.jaberzadeh@gmail.com>
2019-04-12 20:56:41 -0700

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from runner import Experiment
 data = Data('./data/', modalities_to_load=['T1','T2'], dataset='IXI', trim_and_downsample=False)
 data.load()
 
-input_modalities= ['T1','T2']
+input_modalities= ['T1']
 output_weights= { 'T2':1.0, 'concat':1.0}
 exp = Experiment(input_modalities, output_weights, './', data, latent_dim=4, spatial_transformer=True)
 exp.run(data)
